76d221335ee6a56567392a291f19c1e3820c49d3
jiunsu <jiunsu@gmail.com>
2014-04-12 16:53:18 +0900

    jiunsu 수정내용
    1 fib 함수를 메인에서 호출하도록함.
    2 fib함수 내용: 숫자가 2 보다 작을 경우 자신을 return 하고, 아닌 경우 재귀함수로 바로 전, 바로 이전 숫자를 더하여 계산하도록 함.

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,4 +1,4 @@
- public class Fibonacci {
+ public class Fibonacci { 
      public static int fib(int n) {
                  if (n < 2) {
                     return n;
@@ -13,3 +13,8 @@
             System.out.print(fib(i)+", ");
     }    
 }
+
+
+fib 함수를 메인에서 호출하도록함.
+숫자가 2보다 작을 경우 자신을 return 하고
+아닌 경우 재귀함수로 전, 전전 숫자를 더하여 계산함
