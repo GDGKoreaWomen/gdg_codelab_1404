56a71c177d93ce5f3b590d2d46b196ca60ce0693
subin <subin.c.park@gmail.com>
2014-04-12 15:46:48 +0900

    Limit the loop to 93 because of maximum data 'long' can take. Also added commnets.

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,13 +1,23 @@
-public class Fibonacci{
-    public static void main(String []args){
-        int start = Integer.parseInt(args[0]);
-        long fib1 = 0;
-        long fib2 = 1;
-        for(int i = 0; i <= (start -2); i++) {
-            long fib3 = fib1 + fib2;
-            System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
-            fib1 = fib2;
-            fib2 = fib3;
-        }
-    }
+public class Fibonacci {
+	public static void main(String[] args) {
+		int start = Integer.parseInt(args[0]);
+		
+		//Base number
+		long fib1 = 0;
+		long fib2 = 1;
+		
+		//Looping based on argument received
+		for (int i = 0; i <= (start - 2); i++) {
+			//Out of loop at 91(93-2). 91 exceeds the maximum of long data type.
+			if (i >= 91) {
+				break;
+			}
+			//Fibonacci calculation and print out
+			long fib3 = fib1 + fib2;
+			System.out.println("F" + i + " = " + fib1 + ", F" + (i + 1) + " = "
+					+ fib2 + ", F" + (i + 2) + " =  " + fib3);
+			fib1 = fib2;
+			fib2 = fib3;
+		}
+	}
 }
