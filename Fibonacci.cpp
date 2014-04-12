2017505a1b017830665f28a663cf4c9853f6c826
Alex J Hwang <victoryhwang24@gmail.com>
2014-04-12 17:00:23 +0900

    c++ 버젼 피보나치

---
+++ Fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ unsigned long fib1=0, fib2=1, fib3;
+ 
+ cout << "input the index value : "
+ cin >> i
+ cout << iba1 <<"\t" << fib2 << "\t"
+ while(i-2>0){
+  sum = fib1 + fib2;
+  cout << sum << "\t";
+  fib1 = fib2;
+  fib2 = sum;
+  i--;
+ } cout << "\n";
+
+ return 0;
+}
+
