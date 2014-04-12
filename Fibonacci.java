191e6befd7a96af0c2a87c02012fce1b1a691f55
jiunsu <jiunsu@gmail.com>
2014-04-12 16:38:02 +0900

    jiunsu fix2

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,37 +1,15 @@
-public class Fibonacci{
-   
-   /* 원본
-   
-        public static void main(String []args){
-        int start = Integer.parseInt(args[0]);
-        int fib1 = 0;
-        int fib2 = 1;
-        for(int i = 0; i <= (start -2); i++) {
-            int fib3 = fib1 + fib2;
-            System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
-            fib1 = fib2;
-            fib2 = fib3;
-        }
+ public class Fibonacci {
+     public static int fib(int n) {
+                 if (n < 2) {
+                    return n;
+                 }
+                 else {
+           return fib(n-1)+fib(n-2);
+                 }
     }
-    */
-
-    public static long fib(int n) {
-        if (n <= 1) return n;
-        else return fib(n-1) + fib(n-2);
-    }
-
-    public static void main(String[] args) {
-        int start = Integer.parseInt(args[0]);
-        int fib1=0;
-        int fib2=1;
-        
-        for (int i = 0; i <= (start-2); i++) {
-            int fib3 = fib1 + fib2;
-            System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
-            
-            fib1 = fib2;
-            fib2 = fib3;
-        }
-    }
-
+ 
+         public static void main(String[] args) {
+        for (int i=0; i<=46; i++)
+            System.out.print(fib(i)+", ");
+    }    
 }
