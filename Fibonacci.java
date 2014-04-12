bbf21d6e53b8c89f195a1c3af8158ecd312ba098
jiunsu <jiunsu@gmail.com>
2014-04-12 16:26:57 +0900

    jiunsu fix

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,8 +1,9 @@
 public class Fibonacci{
-    public static void main(String []args){
-        int  fib0= 1;
-        int start = Integer.parseInt(fib0);
-        
+   
+   /* 원본
+   
+        public static void main(String []args){
+        int start = Integer.parseInt(args[0]);
         int fib1 = 0;
         int fib2 = 1;
         for(int i = 0; i <= (start -2); i++) {
@@ -12,4 +13,25 @@ public class Fibonacci{
             fib2 = fib3;
         }
     }
+    */
+
+    public static long fib(int n) {
+        if (n <= 1) return n;
+        else return fib(n-1) + fib(n-2);
+    }
+
+    public static void main(String[] args) {
+        int start = Integer.parseInt(args[0]);
+        int fib1=0;
+        int fib2=1;
+        
+        for (int i = 0; i <= (start-2); i++) {
+            int fib3 = fib1 + fib2;
+            System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
+            
+            fib1 = fib2;
+            fib2 = fib3;
+        }
+    }
+
 }
