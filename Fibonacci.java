a684b1e4196e92f5526dfd4be3c8d0602c513a2c
kim jae yeon <7z8x9c@naver.com>
2014-04-12 16:36:07 +0900

    피보나치 부분을 재귀함수로 따로 뺍습니다.

--- Fibonacci.java
+++ Fibonacci.java
@@ -9,18 +9,27 @@ public class Fibonacci{
         long fib1 = 0;
         long fib2 = 1;
         if (start < 2){
-            System.out.println("input number should be larger than 2");
-            return;
+            
         }
         if (start > 92){
             System.out.println("You can't gain over F93");
             start = 92;
         }
         for(long i = 0; i <= (start -2); i++) {
-            long fib3 = fib1 + fib2;
-            System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
-            fib1 = fib2;
-            fib2 = fib3;
+            
+           
+            System.out.println(fibo());
+            
         }
     }
+
+    public static long fibo(long start) {
+        if ( start < 3) {
+            System.out.println("input number should be larger than 2");
+            return 0;
+        } else {
+            fibonacci(start-2) + fibonacci(start-1);
+        }
+    }
+
 }
