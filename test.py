ef8b3b316e3cb155f7db54c462bfc27b6f2ca0e5
bloodevil <bloodevil4@gmail.com>
2014-04-12 16:53:00 +0900

    update python fibonacci and testcode.y

--- test.py
+++ test.py
@@ -1,17 +1,11 @@
+#!/usr/bin/env python
 import commands
-
-def F(n):
-    if n == 0: return 0
-    elif n == 1: return 1
-    else: return F(n-1)+F(n-2)
+from Fibonacci import f
 
 if __name__ == '__main__':
     commands.getstatusoutput('javac Fibonacci.java')
-    for x in range(100):
+    for x in range(2,100):
         java = commands.getstatusoutput('java Fibonacci '+str(x))[1]
-        try:
-            java_last = int(java.split('=')[-1])
-        except:
-            java_last = java.split('=')[-1]
-        if java_last != F(int(x)):
-            print "java %s python %s" % (java_last, F(int(x)))
+        python = f(int(x))
+        if java != python:
+            print "java %s python %s" % (java, python)
