76eb036a5c0cadf019765cb56b89ce2ad1f8dfc2
noirCynical <noircynical@gmail.com>
2014-04-12 16:31:00 +0900

    create c version and modify README

--- Fibonacci.java
+++ Fibonacci.java
@@ -17,20 +17,19 @@ public class Fibonacci{
         long fib1 = 0;
         long fib2 = 1;
         
-        switch(start){
-        case 0:
-            System.out.println("F0 = "+fib1);
-            break;
-        case 1:
-            System.out.println("F0 = "+fib1);
-            break;
-        default:
-            for(int i = 0; i <= (start -2); i++) {
-                long fib3 = fib1 + fib2;
-                System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
-                fib1 = fib2;
-                fib2 = fib3;
-            }
+		if (start < 2){
+            System.out.println("input number should be larger than 2");
+            return;
+        }
+        if (start > 92){
+            System.out.println("You can't gain over F93");
+            start = 92;
+        }
+        for(long i = 0; i <= (start -2); i++) {
+            long fib3 = fib1 + fib2;
+            System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
+            fib1 = fib2;
+            fib2 = fib3;
         }
     }
 }
