61c6997bb607e3ceb3ba22f445ae85a9b43b74ee
MeYou <meyou@caucse.net>
2014-04-12 15:56:22 +0900

    92 초과 숫자를 입력받았을 때

--- Fibonacci.java
+++ Fibonacci.java
@@ -11,6 +11,10 @@ public class Fibonacci{
             System.out.println("input number should be larger than 2");
             return;
         }
+        if (start > 92){
+            System.out.println("You can't gain over F93");
+            start = 92;
+        }
         for(long i = 0; i <= (start -2); i++) {
             long fib3 = fib1 + fib2;
             System.out.prlongln("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
