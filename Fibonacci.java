4cb844c5c3de6e998481ab50169255509989eec5
Hyejin Baek <hj100baek@gmail.com>
2014-04-12 16:15:10 +0900

    start check

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,11 +1,14 @@
 public class Fibonacci{
     public static void main(String []args){
         int start = Integer.parseInt(args[0]);
+        if(start < 2){
+            System.out.println("start <  2  error");
+        }
         int fib1 = 0;
         int fib2 = 1;
-        for(int i = 0; i <= (start -2); i++) {
+        for(int i = 0; i <= (start - 2); i++) {
             int fib3 = fib1 + fib2;
-            System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
+            System.out.println("F" + i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
             fib1 = fib2;
             fib2 = fib3;
         }
