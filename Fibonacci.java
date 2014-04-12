d4970a7566fadb4ba61761faa7370fc770232e6b
MeYou <meyou@caucse.net>
2014-04-12 15:52:12 +0900

    2보다 작은 값이 입력되었을 때 예외처리

--- Fibonacci.java
+++ Fibonacci.java
@@ -7,6 +7,10 @@ public class Fibonacci{
         long start = Integer.parseInt(args[0]);
         long fib1 = 0;
         long fib2 = 1;
+        if (start < 2){
+            System.out.println("input number should be larger than 2");
+            return;
+        }
         for(long i = 0; i <= (start -2); i++) {
             long fib3 = fib1 + fib2;
             System.out.prlongln("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
