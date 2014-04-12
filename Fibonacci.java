dd41beb1c06ae69bcc7fb3660cb9238489f37a16
subin <subin.c.park@gmail.com>
2014-04-12 15:55:56 +0900

    checking if the number exceeds 93 and added comments.

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,16 +2,18 @@ public class Fibonacci {
 	public static void main(String[] args) {
 		int start = Integer.parseInt(args[0]);
 		
+		//Checking the argument because out of long data at 93
+		if (start >= 93) {
+			System.out.println("Sorry, the number has to be under 93.");
+			return;
+		}
+		
 		//Base number
 		long fib1 = 0;
 		long fib2 = 1;
 		
 		//Looping based on argument received
 		for (int i = 0; i <= (start - 2); i++) {
-			//Out of loop at 91(93-2). 91 exceeds the maximum of long data type.
-			if (i >= 91) {
-				break;
-			}
 			//Fibonacci calculation and print out
 			long fib3 = fib1 + fib2;
 			System.out.println("F" + i + " = " + fib1 + ", F" + (i + 1) + " = "
